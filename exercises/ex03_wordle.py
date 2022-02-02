6b915bae4199ff82020caf82964df449ad129bb4
Nicole Mares <nmares@ad.unc.edu>
2022-02-02 16:54:08 -0500

    final version 1

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -3,10 +3,71 @@
 __author__ = "730391824"
 
 
-def contains_char(word: str, letter: str) -> bool:
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+
+# finding letter in word
+def contains_char(secret: str, letter: str) -> bool:
     """if second string (letter) is found within first string (word)."""
     assert len(letter) == 1
-    if (letter == word[0]) or (letter == word[1]) or (letter == word[2]) or (letter == word[3]) or (letter == word[4]):
-        return True
-    else:
-        return False
+    i: int = 0
+    while i < len(secret):
+        if secret[i] == letter:
+            return True
+        i = i + 1 
+    return False
+
+
+# correct boxes for each letter
+def emojified(word: str, secret: str) -> str:
+    """returning correct color for coreect letter"""
+    assert len(word) == len(secret)
+    n: int = 0
+    box: str = ""
+    WHITE_BOX: str = "\U00002B1C"
+    GREEN_BOX: str = "\U0001F7E9"
+    YELLOW_BOX: str = "\U0001F7E8"
+    while n < len(secret):
+        if word[n] == secret[n]:
+            box = box + str(GREEN_BOX)
+        else:
+            if contains_char(secret, word[n]) is True:
+                box = box + str(YELLOW_BOX)
+            else:
+                box = box + str(WHITE_BOX)
+        n = n + 1
+    return box
+
+
+# correct amount of letters in guess
+def input_guess(length: int) -> str:
+    """checking to see if guess is correct length"""
+    guess: str = str(input("Enter a " + str(length) + " character word: "))
+    while len(guess) != length:
+        guess = str(input("That wasn't " + str(length) + " chars! Try again: "))
+    return guess
+
+
+# game loop
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    # Your code will go here
+    turn: int = 1
+    while turn <= 6:
+        print(f"=== Turn {turn}/6 ===")
+        word = (input_guess(5))
+        secret: str = "codes"
+        print(emojified(word, secret))
+        if word == secret:
+            print(f"You won in {turn}/6 turns!")
+            quit()
+        else:
+            turn = turn + 1
+    print("X/6 - Sorry, try again tomorrow!")
+
+
+# converting to module
+if __name__ == "__main__":
+    main()
